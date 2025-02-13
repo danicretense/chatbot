2529e01791655b38fc75f9cdc745a7eb25bb2ad2
Daniela Silva <168598759+danicretense@users.noreply.github.com>
2025-02-13 11:59:06 -0300

    testando com o chromium

--- chatbot.js
+++ chatbot.js
@@ -18,7 +18,7 @@ let client;
         authStrategy: new LocalAuth({ clientId: 'nova-sessao' }),
         puppeteer: {
             headless: true,
-            executablePath:'/usr/bin/google-chrome',
+           // executablePath:'/usr/bin/google-chrome',
             args: ['--no-sandbox',
             '--disable-setuid-sandbox',
             '--disable-dev-shm-usage',
